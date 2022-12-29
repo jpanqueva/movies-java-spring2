844d3ac3dc18279c090a3c43a08323d5da9d5fc2
Jhon Panqueva <jj.panqueva@gmail.com>
2022-12-29 11:10:03 -0500

    first commit

---
+++ src/main/java/com/moviesjpanqueva/moviesjpanqueva/MoviesJpanquevaApplication.java
@@ -0,0 +1,13 @@
+package com.moviesjpanqueva.moviesjpanqueva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesJpanquevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesJpanquevaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviesjpanqueva/moviesjpanqueva/MoviesJpanquevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesjpanqueva.moviesjpanqueva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesJpanquevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
